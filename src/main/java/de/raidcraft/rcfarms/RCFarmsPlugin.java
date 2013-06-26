59acb4f5597a95bccf5eb0923b04b75d1fb4f125
Philip Urban <urban.philip@googlemail.com>
2013-06-26 20:48:11 +0200

    project init

---
+++ src/main/java/de/raidcraft/rcfarms/RCFarmsPlugin.java
@@ -0,0 +1,19 @@
+package de.raidcraft.rcfarms;
+
+import de.raidcraft.api.BasePlugin;
+
+/**
+ * @author Philip Urban
+ */
+public class RCFarmsPlugin extends BasePlugin {
+
+    @Override
+    public void enable() {
+        //TODO: implement
+    }
+
+    @Override
+    public void disable() {
+        //TODO: implement
+    }
+}
