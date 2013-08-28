7ae13d7a179319a7d4d4f116b2c9fb9b1b6960b3
Philip Urban <urban.philip@googlemail.com>
2013-08-28 17:43:11 +0200

    disabled upgrades dependency

--- src/main/java/de/raidcraft/rcfarms/upgrades/FarmRestoreFrequencyUpgrade.java
+++ src/main/java/de/raidcraft/rcfarms/upgrades/FarmRestoreFrequencyUpgrade.java
@@ -1,35 +1,32 @@
 package de.raidcraft.rcfarms.upgrades;
 
-import de.raidcraft.rcupgrades.api.upgrade.AbstractUpgrade;
-import de.raidcraft.rcupgrades.api.upgrade.UpgradeInformation;
-
 /**
  * @author Philip Urban
  */
-@UpgradeInformation(name = "FARM_RESTORE_FREQUENCY")
-public class FarmRestoreFrequencyUpgrade extends AbstractUpgrade {
-
-    private int currentLevel = 0;
-
-    @Override
-    public int getCurrentLevel() {
-
-        return currentLevel;
-    }
-
-    @Override
-    public void increaseLevel() {
-        //TODO: implement
-    }
-
-    @Override
-    public void decreaseLevel() {
-        //TODO: implement
-    }
-
-    @Override
-    public void setLevel(int level) {
-
-        currentLevel = level;
-    }
-}
+//@UpgradeInformation(name = "FARM_RESTORE_FREQUENCY")
+//public class FarmRestoreFrequencyUpgrade extends AbstractUpgrade {
+//
+//    private int currentLevel = 0;
+//
+//    @Override
+//    public int getCurrentLevel() {
+//
+//        return currentLevel;
+//    }
+//
+//    @Override
+//    public void increaseLevel() {
+//        //TODO: implement
+//    }
+//
+//    @Override
+//    public void decreaseLevel() {
+//        //TODO: implement
+//    }
+//
+//    @Override
+//    public void setLevel(int level) {
+//
+//        currentLevel = level;
+//    }
+//}
