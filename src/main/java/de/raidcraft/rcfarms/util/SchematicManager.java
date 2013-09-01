d1e32694bd640f52f511873675dbf7f484afbfb0
Philip Urban <urban.philip@googlemail.com>
2013-09-01 17:35:00 +0200

    fixed deletion regex

--- src/main/java/de/raidcraft/rcfarms/util/SchematicManager.java
+++ src/main/java/de/raidcraft/rcfarms/util/SchematicManager.java
@@ -88,7 +88,7 @@ public class SchematicManager {
     public void deleteSchematic(TFarm tFarm) throws RaidCraftException {
 
         final File folder = getSchematicDir(tFarm.getBukkitWorld());
-        final File[] files = folder.listFiles(new PatternFilenameFilter(SCHEMATIC_PREFIX + tFarm.getId() + "*\\.schematic"));
+        final File[] files = folder.listFiles(new PatternFilenameFilter(SCHEMATIC_PREFIX + tFarm.getId() + ".*\\.schematic"));
 
         if(files.length == 0) {
             RaidCraft.LOGGER.info("No schematic found to delete!");
